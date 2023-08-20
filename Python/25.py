3000c4c9b54a5145a22353179e89bb69c0425d78
Hivan Du <doo@hivan.me>
2023-08-20 19:25:41 +0800

    add 25.ipynb

---
+++ Python/25.py
@@ -0,0 +1,14 @@
+'''
+Author: Hivan Du
+mail: doo@hivan.me
+LastEditors: Hivan Du
+LastEditTime: 2023-08-19 21:13:26
+'''
+# 导入模块
+import matplotlib.pyplot as plt
+# 这段代码会让之后的代码在Jupyter内执行的时候显示图片
+# %matplotlib inline
+# 传入x,y, 通过plot画图
+plt.plot([1,0,9],[4,5,6])
+# 在执行程序的时候显示图形
+plt.show()
